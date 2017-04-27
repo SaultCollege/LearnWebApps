f2147205310855c31f3dd3677600559986a00193
Fred Carella <fred.carella@saultcollege.ca>
2017-04-27 16:25:56 -0400

    Merge origin/master
    Conflicts:
    	src/main/webapp/index.xhtml

--- web/resources/js/app/app.js
+++ web/resources/js/app/app.js
@@ -3,7 +3,8 @@
  * http://www.programming9.com/programs/java/306-design-a-vehicle-class-hierarchy-in-java-to-demonstrate-polymorphism
  */
 /* create a Vehicle object using a constructor function*/
-function Vehicle(make, model, year, image, vin){
+function Vehicle(uuid, make, model, year, image, vin){
+    this.uuid   = uuid;
     this.make   = make,
     this.model  = model,
     this.year   = year;
@@ -17,4 +18,30 @@ function Vehicle(make, model, year, image, vin){
 // Note** use 'this' to access properties
 Vehicle.prototype.toString = function(){return this.make+', '+this.model+', '+this.year;};
 
+function testDiv(uuid, text){
+    var testDiv = document.createElement('DIV');
+    testDiv.style.backgroundColor='grey';
+    testDiv.style.minWidth='600px';
+    testDiv.style.minHeight='400px';
+    testDiv.style.position='fixed';
+    testDiv.style.width='70%';
+    testDiv.style.margin='10% auto';
+    testDiv.style.left='10';
+    testDiv.style.top='0';
+    testDiv.id=uuid;
+    testDiv.innerHTML=text;
+    this.text=text;
+    this.uuid=uuid;
+    this.fn=fn;
+    
+    this.getDiv=function(){
+        return testDiv;
+    };
+    this.onClick=function(fn3){
+        testDiv.onclick=fn3;
+    };
+    this.getId=function(){
+        return uuid;
+    };
+}
 
